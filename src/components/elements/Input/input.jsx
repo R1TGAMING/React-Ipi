b9bc2eafdb8da678360db0858f7d4c30133f2f1d
RTdev <77839017+R1TGAMING@users.noreply.github.com>
2024-07-14 13:27:56 +0000

    Handle submit

--- src/components/elements/Input/input.jsx
+++ src/components/elements/Input/input.jsx
@@ -2,10 +2,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
 export default function InputElement(props) {
-  const {type, placeholder} = props;
+  const {type, placeholder, name, value} = props;
   return (
     <>
-    <input type={type} className="form-control" aria-describedby="emailHelp" placeholder={placeholder} />
+    <input name={name} type={type} className="form-control" placeholder={placeholder} value={value}/>
     </>
   )
 }
--- src/components/pages/login.jsx
+++ src/components/pages/login.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import {useState} from "react";
 
 import Input from "../elements/Input"
 import Button from "../elements/Button"
@@ -8,14 +9,21 @@ import Button from "../elements/Button"
 
 
 export default function Login() {
+  const {value, setValue} = useState('')
+  
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log(value)
+  }
+  
   return (
     <>
-    <form>
+    <form onSubmit = {handleSubmit}>
       <div className="container">
         <div className="d-flex justify-content-center mr-auto ml-auto mt-3">
           <div className="form-group">
-          <Input type = "email" placeholder = "example@email.com " children = "email"/>
-          <Input type = "password" placeholder = "***** " children = "password"/>
+          <Input type = "email" placeholder = "example@email.com " children = "email" name="email" value={(e) => setValue(e)}/>
+          <Input type = "password" placeholder = "***** " children = "password" name="password"/>
           <Button text = "Login"/>
           </div>
         </div>
